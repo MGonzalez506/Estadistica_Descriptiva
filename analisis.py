36ca13997adf0e57ca38cdc10d106461ad3fd8e7
Miguel Gonzalez <dev@mgonzalez506.com>
2021-08-10 20:17:52 -0600

    Agregando el histograma

--- analisis.py
+++ analisis.py
@@ -19,14 +19,16 @@ import re
 import csv
 import numpy as np
 import math
-import matplotlib.pyplot as plot
+#import matplotlib.pyplot as plot
 
 from datetime import datetime, timedelta
 from numpy import array
+from scipy import stats
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 nombre_del_documento = "/energydata_complete.csv"
+#nombre_del_documento = "/data_pag_24_lec_02.csv"
 
 input_dir= str(THIS_FOLDER) + nombre_del_documento
 
@@ -54,32 +56,62 @@ if __name__ == "__main__":
 
 	#Para sacar el máximo de un numpy array: https://numpy.org/doc/stable/reference/generated/numpy.amax.html
 	maximo = np.amax(data_humedad)
+	print("Maximo: {}".format(maximo))
 	minimo = np.amin(data_humedad)
+	print("Minimo: {}".format(minimo))
 	rango = maximo - minimo
 	rango = rango + rango * 0.05
 	print("Rango: {}".format(rango))
 
 	intervalo = rango / num_clases
-	print("Intervalo: {}".format(intervalo))
+	intervalo = round(intervalo,2)
+	print("\n\nIntervalo: {}".format(intervalo))
 
-	#intervalos = range(min(data_humedad), max(data_humedad) + 2)
+	step = 0.009
 
-	intervalos = range(20, 56)
+	inicio_de_limite_de_intervalos = minimo - step
+	print("Inicio del límite: {}".format(inicio_de_limite_de_intervalos))
+	final_de_limite_de_intervalos = maximo + intervalo
+	print("Final del límite: {}\n\n".format(final_de_limite_de_intervalos))
+	array_de_intervalos = []
+	intervalo_temporal = inicio_de_limite_de_intervalos
+	while intervalo_temporal<final_de_limite_de_intervalos:
+		array_de_intervalos.append(intervalo_temporal)
+		intervalo_temporal += intervalo
 
+	print("\n\n -- -- -- La lista de intervalos es -- -- --")
+	cant_intervalos = 0
+	for i in array_de_intervalos:
+		#print(i)
+		cant_intervalos+=1
 
+	print("Cantidad de intervalos: {}\n\n\n".format(cant_intervalos))
 
-	plot.hist(x=data_humedad, bins=[20,25,30,35,40,45,50,55,60], color='#CCFF03', rwidth=0.85)
+	#Función para comparar si dos listas tienen algún dato repetido
+	#Esto con el fin de confirmar que no tengamos datos iguales a un límite	
+	hay_algun_dato_igual = np.in1d(data_humedad,array_de_intervalos)
+	cont = 0
+	indice = 0
+	for i in hay_algun_dato_igual:
+		if i == True:
+			cont += 1
+			print("Este dato está repetido: {}. Y hay {} datos repetidos".format(i,cont))
+			print("El repetido es: {}".format(data_humedad[indice]))
+		indice+=1
+
+	res = stats.relfreq(a, numbins=cant_intervalos)
+	print("\n\nLa frecuencia relativa es:")
+	print(res.frequency)
+	print("------------------------\n\n")
+	
+	"""
+	plot.hist(x=data_humedad, bins=array_de_intervalos, color='#CCFF03', rwidth=0.85)
+	plot.title('- Medidas de humedad en la sala -')
+	plot.xlabel('Valor de Humedad (%)')
+	plot.ylabel('Frecuencia absoluta')
 	plot.show()
 	"""
-	edades = [12, 15, 13, 12, 18, 20, 19, 20, 13, 12, 13, 17, 15, 16, 13, 14, 13, 17, 19]
-	intervalos = range(min(edades), max(edades) + 2)
-	plot.hist(x=edades, bins=intervalos, color='#F2AB6D', rwidth=0.85)
-	plot.title('Histograma de edades - matplotlib - codigopiton.com')
-	plot.xlabel('Edades')
-	plot.ylabel('Frecuencia')
-	plot.xticks(intervalos)
-	plot.show()
-	"""
+	
 	
 
 
